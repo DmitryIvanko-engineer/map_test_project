8f251c07cc3185be92238641be729d3bc807610b
dmitry.ivanko.work@gmail.com <dmitry.ivanko.work@gmail.com>
2023-04-10 20:55:59 +0300

    Beutified

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,21 +1,23 @@
 <template>
   <div>
-    <h1>Find the places!</h1>
-    <div>
-      <label>Enter your address</label>
-      <input type="text" v-model="address" />
+    <div style="width: 40%; margin-left: 30%; margin-right: 30%;">
+      <h1>Find the places!</h1>
+      <div style="display: flex; margin-top: 6px; margin-bottom: 6px;">
+        <label style="width: 50%; display: flex;">Enter your address</label>
+        <input type="text" v-model="address" style="width: 50%; display: flex;"/>
+      </div>
+      <div style="display: flex; margin-top: 6px; margin-bottom: 6px;">
+        <label style="width: 50%; display: flex;">Enter the places you want to search for</label>
+        <input type="text" v-model="place" style="width: 50%; display: flex;"/>
+      </div>
+      <div style="display: flex; margin-top: 6px; margin-bottom: 6px;">
+        <label style="width: 50%; display: flex;">Enter the radius</label>
+        <input type="number" v-model="radius" style="width: 50%; display: flex;"/>
+      </div>
+      <button v-on:click="getMap">
+        Push
+      </button>
     </div>
-    <div>
-      <label>Enter the places you want to search for</label>
-      <input type="text" v-model="place" />
-    </div>
-    <div>
-      <label>Enter the radius</label>
-      <input type="number" v-model="radius" />
-    </div>
-    <button v-on:click="GetMap">
-      Push
-    </button>
     <div id="map" ref="mapDiv" style="width: 100%; height: 80vh" />
   </div>
 </template>
@@ -24,95 +26,36 @@
 /* eslint-disable no-undef */
 import { Options, Vue } from 'vue-class-component';
 import { Loader } from '@googlemaps/js-api-loader';
-//import {googlemaps} from 'googlemaps';
+import MapService, {} from '../services/mapService'
 
 @Options({
   components: {
   },
 })
 export default class SearchForm extends Vue {
-  GOOGLE_MAPS_API_KEY = 'AIzaSyDpyyrjTnWIBVUs14Ebvuvo1Z4xgp44Obw'
-
-  loader = new Loader({ apiKey: this.GOOGLE_MAPS_API_KEY,
-    libraries: ["places"] 
-  })
-  mapDiv: any;
-  map: any;
-
-  coords = {lat: 0, lng: 0} //: google.maps.LatLng = new google.maps.LatLng(0, 0);
+  mapService: any;
   isSupported = 'navigator' in window && 'geolocation' in navigator;
-  watcher: any = null;
-
+  watcher: number | undefined = undefined;
   address = '';
   place = '';
   radius = 0;
 
-  getLocation(position: any) {
-    this.coords.lat = position.coords.latitude; 
-    this.coords.lng = position.coords.longitude;
+  getMap() {
+    this.mapService.GetMap(this.address, this.place, this.radius, this.$refs.mapDiv as HTMLElement);
   }
 
-  mounted() {
-    if (this.isSupported)
-    this.watcher = navigator.geolocation.watchPosition(
-      this.getLocation
-    );
-    this.getCoordinates();
-  }
-
-  async GetMap(): Promise<void> {
-    let address = await this.getCoordinates();
-    this.coords = address === null ? this.coords : address;
-    await this.loader.load();
-    console.log(this.coords);
-    let mapOptions = { center: this.coords, zoom: 15 }
-    let mapDiv: HTMLElement = this.$refs.mapDiv as HTMLElement;
-    let map = new google.maps.Map(mapDiv, mapOptions);
-    const request = {
-      location: this.coords,
-      radius: this.radius,
-      keyword: this.place
-    };
-    var service = new google.maps.places.PlacesService(map);
-
-    function createMarker(place: google.maps.places.PlaceResult, map: google.maps.Map) {
-      if (!place.geometry || !place.geometry.location) return;
-
-      const marker = new google.maps.Marker({
-        map,
-        position: place.geometry.location,
-      });
-
-      //google.maps.event.addListener(marker, "click", () => {
-        //infowindow.setContent(place.name || "");
-        //infowindow.open(map);
-      //});
-    }
-
-    service.nearbySearch(request, function(results, status) {
-      console.log(status);
-      console.log(results);
-      if (status === google.maps.places.PlacesServiceStatus.OK && results !== null) {
-        for (let i = 0; i < results.length; i++) {
-          createMarker(results[i], map)
-        }    
-      }
-    });
-  }
-
-  async getCoordinates() {
-    var requestOptions = {
-      method: 'GET'
-    };
-
-    let response = await fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + this.address.replace(' ', '+') + '&key=' + this.GOOGLE_MAPS_API_KEY, requestOptions);
-    let json = await response.json();
-    let location = json.results[0].geometry.location;
-    return location;
+  async mounted() {
+    let loader = new Loader({ 
+      apiKey: process.env.GOOGLE_MAPS_API_KEY,
+      libraries: ["places"] 
+    })
+    await loader.load();
+    this.mapService = new MapService('navigator' in window && 'geolocation' in navigator);
+    this.watcher = this.mapService.getUserDefaultLocation();
   }
 
   unmounted(): void {
-    if (this.watcher) navigator.geolocation.clearWatch(this.watcher)
+    if (this.watcher) navigator.geolocation.clearWatch(this.watcher);
   }
 
 }
---
+++ src/services/mapService.ts
@@ -0,0 +1,83 @@
+export default class MapService {
+    private coords: google.maps.LatLng;
+    private isSupported: boolean;
+    private mapZoom = 15;
+
+    constructor(isSupported: boolean) {
+        this.coords = new google.maps.LatLng(0);
+        this.isSupported = isSupported;   
+    }
+
+    getUserDefaultLocation(): number | undefined {
+        if (this.isSupported) {
+            const coords = new google.maps.LatLng(0);
+            const watcher = navigator.geolocation.watchPosition(
+                function (position: any) {
+                    coords.lat = position.coords.latitude; 
+                    coords.lng = position.coords.longitude;
+                }
+            );
+            this.coords = coords;
+            return watcher;
+        }
+        return undefined;
+    }
+
+    async GetMap(address: string, place: string, radius: number, mapDiv: HTMLElement): Promise<void> {
+        const addressCoords = await this.getAddressCoordinates(address);
+        const coords: google.maps.LatLng = addressCoords === null ? this.coords : addressCoords;
+
+        console.log(this.coords);
+        
+        const mapOptions = { center: coords, zoom: this.mapZoom }
+        const map = new google.maps.Map(mapDiv, mapOptions);
+        const request = {
+          location: coords,
+          radius: radius,
+          keyword: place
+        };
+        const service = new google.maps.places.PlacesService(map);
+    
+        const createMarker = this.createMarker;
+        service.nearbySearch(request, function (results, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK && results !== null) {
+                for (let i = 0; i < results.length; i++) {
+                    
+                  createMarker(results[i], map)
+                }    
+            }
+        } );
+    }
+
+    private async getAddressCoordinates(address: string): Promise<google.maps.LatLng | null>{
+        if(address !== '' && address !== null) {
+            const requestOptions = {
+                method: 'GET'
+              };
+          
+              const response = await fetch(process.env.GOOGLE_MAPS_API_ADDRESS_PATH + 'address=' + address.replace(' ', '+') + '&key=' + process.env.GOOGLE_MAPS_API_KEY, requestOptions);
+              const json = await response.json();
+              const location: google.maps.LatLng = json.results[0].geometry.location;
+              return location;
+        }
+        else {
+            return null;
+        }
+    }
+
+    private createMarker(place: google.maps.places.PlaceResult, map: google.maps.Map): void {
+        if (!place.geometry || !place.geometry.location) return;
+  
+        const marker = new google.maps.Marker({
+          map,
+          position: place.geometry.location,
+        });
+
+        const infowindow = new google.maps.InfoWindow();
+  
+        google.maps.event.addListener(marker, "click", () => {
+          infowindow.setContent(place.name || "");
+          infowindow.open(map);
+        });
+    }
+}
